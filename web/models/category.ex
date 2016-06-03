333c4e6956c7c9e80effe18563cc8d68323db10a
Jordan Morano <jordan.morano@gmail.com>
2016-06-03 15:54:42 -0400

    Prettify constraint errors to changeset messaging.

--- web/models/category.ex
+++ web/models/category.ex
@@ -20,6 +20,7 @@ defmodule Rumbl.Category do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> unique_constraint(:name)
   end
 
   def alphabetical(query) do
